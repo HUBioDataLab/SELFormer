7a8df91a87223e1be34db0e8355141e38acf4277
Atakan Yüksel <atakanyuksel1997@gmail.com>
2021-10-31 13:21:25 +0300

    initial commit

---
+++ pre_processing.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+chembl_df = pd.read_csv("data/chembl_29_chemreps.txt", sep="\t")
+chembl_df.drop(columns=["standard_inchi", "standard_inchi_key"], inplace=True)
+
+chembl_df["selfies"] = chembl_df["canonical_smiles"]
+chembl_df.head()
+
+import selfies as sf
+def to_selfies(smiles):
+    try:
+        return sf.encoder(smiles)
+    except sf.EncoderError:
+        return smiles
+
+from pandarallel import pandarallel
+pandarallel.initialize()
+chembl_df.selfies = chembl_df.selfies.parallel_apply(to_selfies)
+chembl_df.to_csv("chembl_29_chemreps_selfies.csv", index=False)
+
+
+chembl_df.drop(chembl_df[chembl_df.canonical_smiles == chembl_df.selfies].index, inplace=True)
+chembl_df.to_csv("chembl_29_selfiesonly.csv", index=False)
