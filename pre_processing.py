8e04e168f101170de29a598eab49d37df50f27ff
Atakan Yüksel <atakanyuksel1997@gmail.com>
2021-11-04 14:08:30 +0300

    updating data

--- pre_processing.py
+++ pre_processing.py
@@ -16,8 +16,14 @@ def to_selfies(smiles):
 from pandarallel import pandarallel
 pandarallel.initialize()
 chembl_df.selfies = chembl_df.selfies.parallel_apply(to_selfies)
-chembl_df.to_csv("chembl_29_chemreps_selfies.csv", index=False)
-
 
 chembl_df.drop(chembl_df[chembl_df.canonical_smiles == chembl_df.selfies].index, inplace=True)
-chembl_df.to_csv("chembl_29_selfiesonly.csv", index=False)
+chembl_df.to_csv("chembl_29_selfies.csv", index=False)
+
+selfies_array = chembl_df.selfies.to_numpy(copy=True)
+selfies_alphabet = sf.get_alphabet_from_selfies(selfies_array)
+print(list(selfies_alphabet))
+print(len(selfies_alphabet))
+
+with open("data/chembl_29_selfies_alphabet.txt", "w") as f:
+    f.write(",".join(list(selfies_alphabet)))
