fb982036ab0e99f976e9125e0cd62200117898c2
Atakan Yüksel <atakanyuksel1997@gmail.com>
2022-01-04 16:33:49 +0300

    reformatting and optimizing arguments

--- bpe_tokenizer.py
+++ bpe_tokenizer.py
@@ -1,31 +1,21 @@
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
-
 from tokenizers.pre_tokenizers import Split
 from tokenizers import Regex
-
 from tokenizers.processors import TemplateProcessing
-
 from tokenizers.trainers import BpeTrainer
 
-def bpe_tokenizer(path="./data/selfies_subset.txt"):
 
-    tokenizer = Tokenizer(BPE(unk_token="<unk>"))
+def bpe_tokenizer(path="./data/selfies_subset.txt", save_to="./data/bpe/"):
 
-    tokenizer.pre_tokenizer = Split(pattern=Regex("\[|\]"), behavior="removed")
+	tokenizer = Tokenizer(BPE(unk_token="<unk>"))
 
-    tokenizer.post_processor = TemplateProcessing(
-        single="<s> $A </s>",
-        pair="<s> $A </s> $B:1 </s>:1",
-        special_tokens=[("<s>", 1), ("</s>", 2)],
-    )
+	tokenizer.pre_tokenizer = Split(pattern=Regex("\[|\]"), behavior="removed")
 
-    trainer = BpeTrainer(special_tokens=["<unk>", "<s>", "</s>", "<pad>", "<mask>"])
-    tokenizer.train(files=[path], trainer=trainer)
+	tokenizer.post_processor = TemplateProcessing(single="<s> $A </s>", pair="<s> $A </s> $B:1 </s>:1", special_tokens=[("<s>", 1), ("</s>", 2)],)
 
-    output = tokenizer.encode("[C][=C][C][=C][C][=C][Ring1][=Branch1]")
-    print(output.tokens)
-    print(output.ids)
+	trainer = BpeTrainer(special_tokens=["<unk>", "<s>", "</s>", "<pad>", "<mask>"])
+	tokenizer.train(files=[path], trainer=trainer)
 
-    tokenizer.save("./data/bpe/bpe.json", pretty=True)
-    tokenizer.model.save("./data/bpe/")
+	tokenizer.save(save_to + "bpe.json", pretty=True)
+	tokenizer.model.save(save_to)
--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import pandas as pd
+
 try:
-    chembl_df = pd.read_csv("./data/chembl_29_selfies.csv")
+	chembl_df = pd.read_csv("./data/chembl_29_selfies.csv")
 except FileNotFoundError:
-    import prepare_data
-    prepare_data.prepare_data()
-    chembl_df = pd.read_csv("./data/chembl_29_selfies.csv")
+	import prepare_data
+
+	prepare_data.prepare_data()
+	chembl_df = pd.read_csv("./data/chembl_29_selfies.csv")
 
 
-from os.path import isfile # returns True if the file exists else False.
+from os.path import isfile  # returns True if the file exists else False.
+
 if not isfile("./data/chembl_29_selfies_alphabet.txt"):
-    import create_selfies_alphabet
-    create_selfies_alphabet.get_selfies_alphabet(chembl_df)
+	import create_selfies_alphabet
+
+	create_selfies_alphabet.get_selfies_alphabet(chembl_df)
 
 
 import create_subset
+
 create_subset.create_selfies_subset(chembl_df, 100000)
 
 
 import bpe_tokenizer
+
 bpe_tokenizer.bpe_tokenizer()
 
 
 import roberta_tokenizer
+
 roberta_tokenizer.save_roberta_tokenizer()
+
+import yaml
+
+with open("hyperparameters.yaml") as file:
+	hyperparameters = yaml.load(file)
+	print(hyperparameters)
 
 
 import roberta_model
+
 roberta_model.train_and_save_roberta_model()
 
 
 import test_roberta_model
+
 test_roberta_model.test_roberta_model()
