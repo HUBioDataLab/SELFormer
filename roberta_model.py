55c821c1a9554e065fdad818919000f49b9d7073
Atakan Yüksel <atakanyuksel1997@gmail.com>
2022-02-25 17:11:33 +0300

    HIDDEN_SIZE parameter and logging

--- roberta_model.py
+++ roberta_model.py
@@ -1,20 +1,23 @@
 import torch
 from torch.utils.data.dataset import Dataset
 
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 
 class CustomDataset(Dataset):
-	def __init__(self, df, tokenizer, MAX_LEN):
-		self.examples = []
+    def __init__(self, df, tokenizer, MAX_LEN):
+        self.examples = []
 
-		for example in df.values:
-			x = tokenizer.encode_plus(example, max_length=MAX_LEN, truncation=True, padding="max_length")
-			self.examples += [x.input_ids]
+        for example in df.values:
+            x = tokenizer.encode_plus(example, max_length=MAX_LEN, truncation=True, padding="max_length")
+            self.examples += [x.input_ids]
 
-	def __len__(self):
-		return len(self.examples)
+    def __len__(self):
+        return len(self.examples)
 
-	def __getitem__(self, i):
-		return torch.tensor(self.examples[i])
+    def __getitem__(self, i):
+        return torch.tensor(self.examples[i])
 
 
 import pandas as pd
@@ -29,64 +32,67 @@ from transformers import Trainer, TrainingArguments
 
 import math
 
-
 def train_and_save_roberta_model(hyperparameters_dict, selfies_path="./data/selfies_subset.txt", robertatokenizer_path="./data/robertatokenizer/", save_to="./saved_model/"):
-	TRAIN_BATCH_SIZE = hyperparameters_dict["TRAIN_BATCH_SIZE"]
-	VALID_BATCH_SIZE = hyperparameters_dict["VALID_BATCH_SIZE"]
-	TRAIN_EPOCHS = hyperparameters_dict["TRAIN_EPOCHS"]
-	LEARNING_RATE = hyperparameters_dict["LEARNING_RATE"]
-	WEIGHT_DECAY = hyperparameters_dict["WEIGHT_DECAY"]
-	MAX_LEN = hyperparameters_dict["MAX_LEN"]
+    TRAIN_BATCH_SIZE = hyperparameters_dict["TRAIN_BATCH_SIZE"]
+    VALID_BATCH_SIZE = hyperparameters_dict["VALID_BATCH_SIZE"]
+    TRAIN_EPOCHS = hyperparameters_dict["TRAIN_EPOCHS"]
+    LEARNING_RATE = hyperparameters_dict["LEARNING_RATE"]
+    WEIGHT_DECAY = hyperparameters_dict["WEIGHT_DECAY"]
+    MAX_LEN = hyperparameters_dict["MAX_LEN"]
+    
 
-	config = RobertaConfig(
-		vocab_size=hyperparameters_dict["VOCAB_SIZE"], 
-		max_position_embeddings=hyperparameters_dict["MAX_POSITION_EMBEDDINGS"], 
-		num_attention_heads=hyperparameters_dict["NUM_ATTENTION_HEADS"], 
-		num_hidden_layers=hyperparameters_dict["NUM_HIDDEN_LAYERS"], 
-		type_vocab_size=hyperparameters_dict["TYPE_VOCAB_SIZE"]
-		)
+    config = RobertaConfig(
+        vocab_size=hyperparameters_dict["VOCAB_SIZE"], 
+        max_position_embeddings=hyperparameters_dict["MAX_POSITION_EMBEDDINGS"], 
+        num_attention_heads=hyperparameters_dict["NUM_ATTENTION_HEADS"], 
+        num_hidden_layers=hyperparameters_dict["NUM_HIDDEN_LAYERS"], 
+        type_vocab_size=hyperparameters_dict["TYPE_VOCAB_SIZE"],
+        hidden_size=hyperparameters_dict["HIDDEN_SIZE"]
+        )
 
-	model = RobertaForMaskedLM(config=config)
-	df = pd.read_csv(selfies_path, header=None)
+    #model = RobertaForMaskedLM(config=config)
+    def _model_init():
+        return RobertaForMaskedLM(config=config)
 
-	tokenizer = RobertaTokenizerFast.from_pretrained(robertatokenizer_path)
+    df = pd.read_csv(selfies_path, header=None)
 
-	train_df, eval_df = train_test_split(df, test_size=0.2, random_state=42)
-	train_dataset = CustomDataset(train_df[0], tokenizer, MAX_LEN)  # column name is 0.
-	eval_dataset = CustomDataset(eval_df[0], tokenizer, MAX_LEN)
+    tokenizer = RobertaTokenizerFast.from_pretrained(robertatokenizer_path)
 
-	data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
+    train_df, eval_df = train_test_split(df, test_size=0.2, random_state=42)
+    train_dataset = CustomDataset(train_df[0], tokenizer, MAX_LEN)  # column name is 0.
+    eval_dataset = CustomDataset(eval_df[0], tokenizer, MAX_LEN)
 
-	training_args = TrainingArguments(
-		output_dir=save_to,
-		overwrite_output_dir=True,
-		evaluation_strategy="epoch",
-		save_strategy="epoch", # saves after each epoch
-		num_train_epochs=TRAIN_EPOCHS,
-		learning_rate=LEARNING_RATE,
-		weight_decay=WEIGHT_DECAY,
-		per_device_train_batch_size=TRAIN_BATCH_SIZE,
-		per_device_eval_batch_size=VALID_BATCH_SIZE,
-		# fp16=True,
-		save_total_limit=1, # keeps only 1 checkpoint
-		disable_tqdm=True, # removes progress bars
-		load_best_model_at_end=True, # loads the best model achieved during the training after training is finished.
-	)
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
 
-	trainer = Trainer(
-		model=model,
-		args=training_args,
-		data_collator=data_collator,
-		train_dataset=train_dataset,
-		eval_dataset=eval_dataset,
-		#prediction_loss_only=True,
-	)
+    training_args = TrainingArguments(
+        output_dir=save_to,
+        overwrite_output_dir=True,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        num_train_epochs=TRAIN_EPOCHS,
+        learning_rate=LEARNING_RATE,
+        weight_decay=WEIGHT_DECAY,
+        per_device_train_batch_size=TRAIN_BATCH_SIZE,
+        per_device_eval_batch_size=VALID_BATCH_SIZE,
+        save_total_limit=1,
+        disable_tqdm=True,
+        # fp16=True
+    )
 
-	print("build trainer with on device:", training_args.device, "with n gpus:", training_args.n_gpu)
+    trainer = Trainer(
+        model_init=_model_init,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        # prediction_loss_only=True,
+    )
 
-	trainer.train()
-
-	eval_results = trainer.evaluate()
-	print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
-	
-	trainer.save_model(save_to)
+    print("build trainer with on device:", training_args.device, "with n gpus:", training_args.n_gpu)
+    trainer.train()
+    print("training finished.")
+    
+    eval_results = trainer.evaluate()
+    print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+    
+    trainer.save_model(save_to)
