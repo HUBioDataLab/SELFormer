e3e28f4f8b5cd97b892f2427eddd89454217dd6b
Atakan Yüksel <atakanyuksel1997@gmail.com>
2022-05-03 17:17:50 +0300

    No change.

--- roberta_tokenizer.py
+++ roberta_tokenizer.py
@@ -1,9 +1,8 @@
 from transformers import RobertaTokenizerFast
 
-
 def save_roberta_tokenizer(path="./data/bpe/", save_to="./data/robertatokenizer/"):
 	tokenizer = RobertaTokenizerFast.from_pretrained(path)
-	print("Loaded pretrained BPE tokenizer from: " + path)
+	print("Loaded BPE Tokenizer from: " + path)
 
 	tokenizer.save_pretrained(save_to)
 	print("Saved RobertaTokenizerFast to: " + save_to)
