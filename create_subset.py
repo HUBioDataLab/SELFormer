2d6e5c6906f77f3f71276ef5d0e8239cdfe747ae
AtakanYuksel <48133201+AtakanYuksel@users.noreply.github.com>
2022-05-10 17:05:46 +0300

    Delete create_subset.py

--- create_subset.py
+++
@@ -1,8 +0,0 @@
-def create_selfies_subset(selfies_df, save_to="./data/selfies_subset.txt", subset_size=100000):
-	selfies_df.sample(frac=1).reset_index(drop=True)  # shuffling
-
-	selfies_subset = selfies_df.selfies[:subset_size]
-	selfies_subset = selfies_subset.to_frame()
-	selfies_subset["selfies"].to_csv(save_to, index=False, header=False)
-
-	print("Subset size {} saved to: {}".format(subset_size, save_to))
