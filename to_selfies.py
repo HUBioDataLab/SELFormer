c8fa1aab38318f7a7f0058db7ea476e31cfe067a
AtakanYuksel <48133201+AtakanYuksel@users.noreply.github.com>
2022-05-10 17:06:29 +0300

    Delete to_selfies.py

--- to_selfies.py
+++
@@ -1,8 +0,0 @@
-import selfies as sf
-
-
-def to_selfies(smiles):  # returns selfies representation of smiles string. if there is no representation return smiles unchanged.
-    try:
-        return sf.encoder(smiles)
-    except sf.EncoderError:
-        return smiles
