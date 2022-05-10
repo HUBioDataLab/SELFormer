6ed10c983ae64e6b4cf2c8a3998c6bc42154a65f
AtakanYuksel <48133201+AtakanYuksel@users.noreply.github.com>
2022-05-10 17:06:19 +0300

    Delete roberta_tokenizer.py

--- roberta_tokenizer.py
+++
@@ -1,9 +0,0 @@
-from transformers import RobertaTokenizerFast
-
-
-def save_roberta_tokenizer(path="./data/bpe/", save_to="./data/robertatokenizer/"):
-	tokenizer = RobertaTokenizerFast.from_pretrained(path)
-	print("Loaded pretrained BPE tokenizer from: " + path)
-
-	tokenizer.save_pretrained(save_to)
-	print("Saved RobertaTokenizerFast to: " + save_to)
